c6e1a053717aaf889629805d4dd727c9ac27b92a
Harshal <attardeharshal94@gmail.com>
2026-01-15 22:53:10 +0530

    Windows

--- src/test/java/CreateLocation.java
+++ src/test/java/CreateLocation.java
@@ -14,7 +14,7 @@ DynamicJson ds= new DynamicJson();
 
 
     void createLocationAdmin(String                 locationName,String locationDesc,int locationPhone,String locationCode){
-        RestAssured.baseURI=        "https://apiv2.stage-hubengage.com";
+        RestAssured.baseURI    =        "https://apiv2.stage-hubengage.com";
         given().header      ("Authorization","")
                 .header("content-type","application/json")
                 .header("tenant","")
